fc108193e2a473ddb90f3dcd0126d354ddc5b6fe
Casas Jr <gideon.s.casas.jr@accenture.com>
2019-11-06 10:56:17 +0800

    Updates

---
+++ precache-manifest.c77388c7df04990a5d243b57d3797c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581f907c58e94b57ecfbd3d73c931e90",
+    "url": "/album/index.html"
+  },
+  {
+    "revision": "8c8bf5d68c153443cea7",
+    "url": "/album/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6400f0280f5044c8fd19",
+    "url": "/album/static/js/2.aa0d2d53.chunk.js"
+  },
+  {
+    "revision": "8c8bf5d68c153443cea7",
+    "url": "/album/static/js/main.75a1ff65.chunk.js"
+  },
+  {
+    "revision": "716062a98fff18fd55da",
+    "url": "/album/static/js/runtime-main.2c07235d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/album/static/media/logo.25bf045c.svg"
+  }
+]);
